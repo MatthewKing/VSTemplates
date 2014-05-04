61955df8e35344baf66d11f179fa18327b479195
Matthew King <mking@mking.io>
2014-05-04 13:31:17 +0800

    Added 'class' item template.

---
+++ source/Class.cs
@@ -0,0 +1,12 @@
+﻿namespace $rootnamespace$
+{
+    using System;
+    using System.Collections.Generic;
+    $if$ ($targetframeworkversion$ >= 3.5)using System.Linq;
+    $endif$using System.Text;
+
+    internal sealed class $safeitemrootname$
+    {
+
+    }
+}
